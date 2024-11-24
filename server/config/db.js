663ebb5e3f9c59848ba3b7fa9a629c78b9561196
Andres suarez <suarezruizandresfelipe801@gmail.com>
2024-11-24 16:46:44 -0500

    env

--- server/config/db.js
+++ server/config/db.js
@@ -1,9 +1,10 @@
-import { Sequelize } from "sequelize";
+import { Sequelize, DataTypes } from "sequelize";
 import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
 export const sequelize = new Sequelize(process.env.DB_SCHEMA, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
-    dialect: "mysql"
-})
+    port: process.env.DB_PORT,
+    dialect: "mysql",
+});
