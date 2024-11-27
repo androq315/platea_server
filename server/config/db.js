18fbd2b9daeaef024550abdcbf9c1611cdff4181
Danner1233 <dannerariasmunive11@gmail.com>
2024-11-26 23:11:10 -0500

    Localhost cambios

--- server/config/db.js
+++ server/config/db.js
@@ -1,19 +1,9 @@
-import { Sequelize, DataTypes } from "sequelize";
+import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config()
 
 export const sequelize = new Sequelize(process.env.DB_SCHEMA, process.env.DB_USER, process.env.DB_PASS, {
-<<<<<<< HEAD
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  dialect: "mysql",
-  dialectOptions: {
-    connectTimeout: 60000, // Ajustar si el proxy es lento
-  },
-=======
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: "mysql",
->>>>>>> ae2dffe842265c4911cb5f6de5dde607e9fcef46
-});
+    dialect: "mysql"
+})
